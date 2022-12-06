6bc9496d98a2d311cb509d30ea54f11db796fce5
dyrteg <61887221+hotveyn@users.noreply.github.com>
2022-12-06 11:30:28 +0300

    Create AirportItem.vue

---
+++ src/components/search/AirportItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="airport">
+    <p>Аэропорт - {{props.airport.name}}</p>
+    <p>Iata аэропорта - {{props.airport.iata }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import IAirport from "@/interfaces/IAirport";
+
+const props = defineProps<{
+  airport: IAirport
+}>();
+</script>
+
+<style scoped lang="scss">
+
+</style>
